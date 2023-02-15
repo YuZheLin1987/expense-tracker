712ad70fef69ef174cd0ddeac6d57294c01273ba
YuZheLin1987 <billlinxx@gmail.com>
2023-02-16 01:13:56 +0800

    feat: add mongoose, category & record model

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const app = express()
+const mongoose = require('mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
+const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
 const port = 3000
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
